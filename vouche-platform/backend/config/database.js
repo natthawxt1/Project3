b82c1ecb46fce735006a4264bca6201fa5663b4e
KaminSirisu <Kaminsirisu@gmail.com>
2025-11-14 15:42:54 +0700

    kuy

--- vouche-platform/backend/config/database.js
+++ vouche-platform/backend/config/database.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 8889,
+  port: process.env.DB_PORT || 3306, //8889
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
